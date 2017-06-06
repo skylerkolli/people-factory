c083b5386b821d203362b857a6170b0c21caec62
skylerkolli <skylerkolli@gmail.com>
2017-06-06 14:50:27 -0400

    add git

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const personForm = document.querySelector('#personForm')
-const foodForm = document.querySelector('#foodForm')
+//const foodForm = document.querySelector('#foodForm')
 function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
-    const heading=document.querySelector("h1")
-    const paragraph=document.querySelector('#foodForm')
-    console.log(f.personName.value)
-    heading.textContent = f.personName.value + " favorite food is "+ f.foodName.value
-    paragraph.textContent = f.foodName.value + " is " + f.personName.value + " favorite food"
-    paragraph.style.fontSize= smaller
+    const details = document.querySelector("#details")
+    const name = f.personName.value
+    const favoriteColor = f.favoriteColor.value
+    const age = f.age.value
+
+    const colorDiv = `<div></div>`
+    //const heading=document.querySelector("h1")
+   // const paragraph=document.querySelector('#foodForm')
+    //console.log(f.personName.value)
+//     const boldedName = document.createElement("strong")
+//     boldedName.textContent = name
+//     heading.textContent = f.personName.value + " favorite food is "+ f.foodName.value
+//     paragraph.textContent = f.foodName.value + " is " + f.personName.value + " favorite food"
+//     paragraph.style.fontSize= smaller
+//     details.appendChild(boldedName)
+    details.innerHTML =`
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Favorite Color: ${favoriteColor}</li>
+        <li>Age:${age}</li>
+    </ul>`
 }
 personForm.addEventListener("submit", handleSubmit)
 
